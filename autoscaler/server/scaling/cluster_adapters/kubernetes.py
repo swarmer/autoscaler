e52f1f007937b064ba58b303c3fcebdaa486afdb
Anton Barkovsky <anton@swarmer.me>
2016-03-25 08:30:04 +0300

    Implement kubernetes cluster adapter

--- autoscaler/server/scaling/cluster_adapters/kubernetes.py
+++ autoscaler/server/scaling/cluster_adapters/kubernetes.py
@@ -1,6 +1,30 @@
+import requests
+import warnings
+
+
 class KubernetesClusterAdapter:
     def __init__(self, cluster_config):
-        pass
+        self.api_host = cluster_config['kubernetes_api_host']
+        self.user = cluster_config['kubernetes_user']
+        self.password = cluster_config['kubernetes_password']
+        self.rc_name = cluster_config['kubernetes_rc_name']
 
     def scale(self, instance_count):
-        pass
+        scale_url = 'https://%s/api/v1/namespaces/default' \
+                    '/replicationcontrollers/hello/scale' % self.api_host
+        data = {
+            'metadata': {
+                'namespace': 'default',
+                'name': self.rc_name,
+            },
+            'spec': {
+                'replicas': 2,
+            }
+        }
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            requests.put(
+                scale_url, json=data,
+                auth=(self.user, self.password), verify=False
+            )
